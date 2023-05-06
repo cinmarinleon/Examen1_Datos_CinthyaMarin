2d7ea51065d563a9b7b7c63dca75cc74839ed45c
Cinthya <cinmarinleon@gmail.com>
2023-05-05 22:50:14 -0600

    Datos aplanados

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
 import utilitarios
 from utilitarios import *
 
-#
-# unPaciente = {
-#     "idPaciente": utilitarios.generar_id_pacientes(),
-#     "edad": utilitarios.generar_numero_aleatorio(0,1200),
-#     "condicion": utilitarios.generar_estado_salud(),
-#
-# }
-#
-# unaCelula = {
-#     "idCelula": utilitarios.generar_id_celula(),
-#     "analista": utilitarios.generar_id_analista(),
-#     "radioX": utilitarios.generar_numero_aleatorio(5,99),
-#     "radioY": utilitarios.generar_numero_aleatorio(5,99),
-#     "radioZ": utilitarios.generar_numero_aleatorio(5,99),
-#
-# }
-
 # Generar las celulas
 
 idCelulasString = utilitarios.generar_lista_ids_celulas()
@@ -25,11 +8,11 @@ idCelulasString = utilitarios.generar_lista_ids_celulas()
 pilaCelulas = []
 for celula in idCelulasString:
     unaCelula = {
-        "idCelula": celula,
-        "analista": utilitarios.generar_id_analista(),
-        "radioX": utilitarios.generar_numero_aleatorio(5,99),
-        "radioY": utilitarios.generar_numero_aleatorio(5,99),
-        "radioZ": utilitarios.generar_numero_aleatorio(5,99),
+        "ID_CELULA": celula,
+        "COD_ANALISTA": utilitarios.generar_id_analista(),
+        "DIM_RAD_NM_X": utilitarios.generar_numero_aleatorio(5,99),
+        "DIM_RAD_NM_Y": utilitarios.generar_numero_aleatorio(5,99),
+        "DIM_RAD_NM_Z": utilitarios.generar_numero_aleatorio(5,99),
     }
     pilaCelulas.append(unaCelula)
 
@@ -40,9 +23,9 @@ idPacientesString = utilitarios.generar_lista_ids_pacientes()
 listaPacientes = []
 for paciente in idPacientesString:
     unPaciente = {
-        "idPaciente": paciente,
-        "edad": utilitarios.generar_numero_aleatorio(0,1200),
-        "condicion": utilitarios.generar_estado_salud(),
+        "COD_PACIENTE": paciente,
+        "EDAD_PACIENTE": utilitarios.generar_numero_aleatorio(0,1200),
+        "COND_GENERAL_PACIENTE": utilitarios.generar_estado_salud(),
         "celulas": []
     }
     listaPacientes.append(unPaciente)
@@ -56,6 +39,23 @@ for celula in pilaCelulas:
     paciente = random.choice(listaPacientes)
     paciente["celulas"].append(celula)
 
- 
+
+datosAplanados = []
+for paciente in listaPacientes:
+    for celula in paciente["celulas"]:
+        unPaciente = {
+            "ID_CELULA": celula["ID_CELULA"],
+            "COD_ANALISTA": celula["COD_ANALISTA"],
+            "DIM_RAD_NM_X": celula["DIM_RAD_NM_X"],
+            "DIM_RAD_NM_Y": celula["DIM_RAD_NM_Y"],
+            "DIM_RAD_NM_Z": celula["DIM_RAD_NM_Z"],
+            "COD_PACIENTE": paciente["COD_PACIENTE"],
+            "COND_GENERAL_PACIENTE": paciente["COND_GENERAL_PACIENTE"],
+            "EDAD_PACIENTE": paciente["EDAD_PACIENTE"],
+        }
+        datosAplanados.append(unPaciente)
+
+
+
 
 
